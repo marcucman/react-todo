2e6447d4c72fd3b15ebdf5dd64463cdefca4253c
Marc Ucman <marcucman@yahoo.com>
2017-03-14 18:44:56 -0500

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -29,5 +29,48 @@ module.exports = {
     // }
     // this does the same as above but it more concise
     return $.isArray(todos) ? todos : []; // return the array if true, return a blank array if false
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      // return each todo either if not completed, or showCompleted is checked to be true
+      return !todo.completed || showCompleted;
+    });
+
+    // // Filter by searchText
+    // if (searchText.length > 0) {
+    //   var searchText = searchText.toLowerCase();
+    //   filteredTodos = filteredTodos.filter((todo) => {
+    //     return todo.text.indexOf(searchText) >= 0;
+    //   });
+    // }
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+
+      // if (searchText.length > 0) { // not needed, since .toLowerCase() is done in TodoApp
+      //   searchText = searchText.toLowerCase();
+      // }
+
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        // if a is not completed and b is, put b in front of a
+        return -1;
+      } else if (a.completed && !b.completed) {
+        // if a is completed and b is not, put a in front of b
+        return 1;
+      } else {
+        // both a and b have the same completed status, and don't need to move
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -57,4 +57,50 @@ describe('TodoAPI', () => {
       expect(actualTodos).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () => {
+    var todos = [{
+      id: 1,
+      text: 'First test todo here',
+      completed: true
+    }, {
+      id: 2,
+      text: 'Second test todo here',
+      completed: false
+    }, {
+      id: 3,
+      text: 'Third test todo',
+      completed: true
+    }];
+
+    it('should return all items if showCompleted is true', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+
+    it('should return non-completed todos if showCompleted is false', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filteredTodos.length).toBe(1);
+    });
+
+    it('should sort by completed status', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos[0].completed).toBe(false);
+    });
+
+    it('should filter todos by searchText', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, 'here');
+
+      expect(filteredTodos.length).toBe(2);
+    });
+
+    it('should return all todo if searchText is empty', () => {
+      var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filteredTodos.length).toBe(3);
+    });
+  });
 });
