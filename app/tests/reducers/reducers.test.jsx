ffc8344dad2080d42edca09c496ebe6ef5e1ec54
Marc Ucman <marcucman@yahoo.com>
2017-03-17 02:01:24 -0500

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -26,4 +26,41 @@ describe('Reducers', () => {
       expect(res).toEqual(true);
     });
   });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle completed and assign completedAt a value for todo', () => {
+      var todos = [{
+        id: 1,
+        text: "Do something",
+        completed: false,
+        createdAt: 44323,
+        completedAt: undefined
+      }, {
+        id: 2,
+        text: "Do something else",
+        completed: true,
+        createdAt: 44753,
+        completedAt: 44834
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(true);
+      expect(res[0].completedAt).toNotEqual(undefined);
+    });
+  });
 });
