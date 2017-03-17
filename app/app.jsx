6bff8dcf63bdfd2550565856698eff5ea7400af1
Marc Ucman <marcucman@yahoo.com>
2017-03-17 02:12:07 -0500

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,18 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var TodoApp = require('TodoApp');
 
+// REQUIRE REDUX FUNCTIONALITY
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Clean the yard'));
+store.dispatch(actions.setSearchText('yard'));
+store.dispatch(actions.toggleShowCompleted());
+
 // Load foundation, not needed after including sassLoader to webpack.config.js
 // require('style!css!foundation-sites/dist/foundation.min.css'); // use style-loader and css-loader module
 $(document).foundation(); // attach foundation to document
